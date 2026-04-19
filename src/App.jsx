f096ad7746a984c7b2078244c2cdba4f27fef371
CKCarr <119520583+CKCarr@users.noreply.github.com>
2026-04-19 01:05:46 -0500

    App updated conditional logic for state of Chat renders UI of selected state based on functions to start and end chat, stored in props to pass inline

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatBotStart from './Components/ChatBotStart'
 import ChatBotApp from './Components/ChatBotApp'
 
 const App = () => {
+  const [isChatting, setIsChatting] = useState(false)
+
+  const handleStartChat = () => {
+    setIsChatting(true);
+  };
+
+  const handleEndChat = () => {
+    setIsChatting(false);
+  };
+
   return (
     <div className='container'>
-      {/* <ChatBotStart /> */}
-      <ChatBotApp />
+      {/* Logic conditional state of Chat renders UI of selected state */}
+      {isChatting ? <ChatBotApp onGoBack={handleEndChat} /> : <ChatBotStart onStartChat={handleStartChat} />}
     </div>
   )
-}
+};
 
 export default App
 
